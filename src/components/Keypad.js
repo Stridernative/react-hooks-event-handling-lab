95ba83cf91b761ec659e7780d1e511982aaea41a
Gabe <GabeCreaCS@gmail.com>
2021-09-03 08:26:49 -0700

    Completed assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import React from 'react'
+
+
+
+export const Keypad = () => {
+     const typingMessage = () => {
+        console.log('Entering password...')
+    }
+    return (
+        <div>
+            <input type="password" onChange={() => typingMessage()}/>
+        </div>
+    )
+}
